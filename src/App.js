bbd0fe03c87d8e5d11f25fb034c3f996faa50bcb
Confidence Madiope <confidencem97@gmail.com>
2022-09-12 20:00:37 +0200

    ADD: menu

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import { ThemeProvider } from "styled-components";
 import { GlobalStyles } from "./global";
 import { theme } from "./theme";
+import { Burger } from "./components";
+import { useState } from "react";
 
 function App() {
+  const [open, setOpen] = useState(false);
   return (
     <ThemeProvider theme={theme}>
-    <GlobalStyles/>
+      <GlobalStyles />
+
+      <Burger open={open} setOpen={setOpen} aria-controls={menuId}  />
+
       <div className='main-text'>
         <h1>hello world</h1>
       </div>
